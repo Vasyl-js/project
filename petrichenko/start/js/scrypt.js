4d25b31e729b59cd091d0934cd8f6230efe08102
Vasyl <kvgrestdon@gmail.com>
2021-04-20 17:36:00 +0200

    first commit

---
+++ petrichenko/start/js/scrypt.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+const answers = [];
+
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your age?', '');
+// answers[2] = prompt('what is your  second name?', '');
+
+console.log(typeof(answers));
